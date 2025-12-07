fb35579058e3ebafff8bed9ff729ff048575aca3
SurbhiJadav <jadavsubhu04@outlook.com>
2025-12-07 21:49:10 +0530

    pages

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,7 @@
+export const About = () => {
+    return(
+        <>
+            <h1>This is About Page!!</h1>
+        </>
+    )
+}
---
+++ client/src/pages/Service.jsx
@@ -0,0 +1,7 @@
+export const Service = () => {
+    return(
+        <>
+            <h1>This is Service Page!!</h1>
+        </>
+    )
+}
